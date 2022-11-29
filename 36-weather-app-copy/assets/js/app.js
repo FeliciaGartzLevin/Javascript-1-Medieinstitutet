33a9a9de0a51a311cf746d2dc10aec5e37f8b1b4
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-11-29 15:41:35 +0100

    succeeded to hide/show error message and forecast card

--- 36-weather-app-copy/assets/js/app.js
+++ 36-weather-app-copy/assets/js/app.js
@@ -5,4 +5,54 @@
 
 //  'https://api.openweathermap.org/data/2.5/weather?q=Malm%C3%B6&units=metric&appid=dd1addbd5798549d54ed647f75db5af1'
 
-document.querySelector('#search-form');
+const renderCurrentWeather = data => {
+
+    document.querySelector('#forecast').innerHTML = `
+    <div class="card">
+				<img src="assets/images/forecast-banner.png" class="card-img-top">
+				<div class="card-body">
+					<h5 class="card-title" id="location">
+						<span id="city">${data.name}</span>,
+						<span id="country">${data.sys.country}</span>
+					</h5>
+					<p class="temp">
+						<span id="temperature">${data.main.temp}</span>
+						&deg;C
+					</p>
+					<p class="humidity">
+						<span id="humidity">${data.main.humidity}</span>
+						&percnt; humidity
+					</p>
+					<p class="wind">
+						<span id="windspeed">${data.wind.speed}</span>
+						m/s
+					</p>
+				</div>
+			</div>
+    `;
+
+}
+
+document.querySelector('#search-form').addEventListener('submit', async e => {
+    e.preventDefault();
+
+    // kan targeta på båda nedan sätt
+    // document.querySelector('#search-form').value;
+    const city = e.target.query.value.trim();
+
+    if(city.lenght > 3){
+
+        alert("Please enter at least 3 chars");
+        // return i en if-sats gör att vi inte behöver nästla if-sats. 
+        // den går ur satsen och går vidare till nästa kodblock.
+        return;
+    }
+    
+    // do search
+    console.log(`Searching for city: ${city}`);
+    const data = await getCurrentWeather(city);
+
+    // render current weather conditions
+    renderCurrentWeather(data);
+
+});
--- 36-weather-app-copy/assets/js/owmapi.js
+++ 36-weather-app-copy/assets/js/owmapi.js
@@ -5,20 +5,45 @@
 const API_KEY = "dd1addbd5798549d54ed647f75db5af1";
 const BASE_URL= "https://api.openweathermap.org/data/2.5";
 
+const FAKE_SLOW_API = true;
+const FAKE_SLOW_API_DELAY = 3000;
+
 const getCurrentWeather = async city => {
-    // get weather for coty from OWM API
+    const errorEl = document.querySelector('#error');
+    const forecast = document.querySelector('#forecast');
+	errorEl.classList.add('hide');
+
+    try{
+    // get weather for city from OpenWeatherMap API
     const response = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
+        
+        // check if response is ok
+        if(!response.ok){
+            throw new Error(`We cannot find that city. Problem is: ${response.status} ${response.statusText}`);
+        }
+        // convert response from JSON
+        const data = await response.json();
 
-    // check if response is ok
-    if(!response.ok){
-        throw new Error(`${response.status} ${response.statusText}`)
+        // fake a slow api
+	    FAKE_SLOW_API && await new Promise(r => setTimeout(r, FAKE_SLOW_API_DELAY));
+       
+        // show forecast card
+        forecast.classList.remove('hide');
+
+        // return current weather
+        return data;
+
+    } catch (err) {
+        console.log('Caught the error.', err);
+
+        errorEl.innerText = err;
+        errorEl.classList.remove('hide');
+        forecast.classList.add('hide');
+        
     }
-    // convert response from JSON
-    const data = await response.json();
-
-    // return current weather
-    return data;
 }
+
+
 
 
 
