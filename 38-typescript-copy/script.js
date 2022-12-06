491b4d3421d98253763d8509859e94197b07ff96
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2022-12-06 13:51:33 +0100

    downloaded Vite and trying it

--- 38-typescript-copy/script.js
+++ 38-typescript-copy/script.js
@@ -6,7 +6,26 @@
  *
  * 'tsc -w script.ts'
  * 'ctrl C' to end watch
+ *
+ * Fördelen med TypeScript är att
+ * felet fångas direkt istället för att
+ * vi inte ska se felet förrän i browsern
  */
 console.log('Hello Typescript');
 console.log('Hello again Typescript');
 console.log('And again!');
+var johan = 'Johan';
+johan.toUpperCase();
+var level = 1337;
+level = 42;
+var x;
+x = "lol";
+// x = 42; //nope
+var b;
+var todos = ["Learn JS", "Learn TS"];
+todos.push("Learn stuff");
+// todos.push(42); // nope
+var names = [];
+console.log(names);
+var points = [];
+console.log(points);
--- 38-typescript-copy/script.ts
+++ 38-typescript-copy/script.ts
@@ -6,8 +6,35 @@
  *
  * 'tsc -w script.ts'
  * 'ctrl C' to end watch
+ * 
+ * Fördelen med TypeScript är att 
+ * felet fångas direkt istället för att 
+ * vi inte ska se felet förrän i browsern
  */
 
  console.log('Hello Typescript');
  console.log('Hello again Typescript');
  console.log('And again!');
+
+let johan = 'Johan';
+johan.toUpperCase();
+
+let level = 1337;
+level = 42;
+
+let x: string;
+x = "lol";
+// x = 42; //nope
+
+let b: false;
+
+let todos = ["Learn JS", "Learn TS"];
+todos.push("Learn stuff");
+// todos.push(42); // nope
+
+let names: string[] = [];
+console.log(names);
+
+let points: number[] = [];
+console.log(points);
+
